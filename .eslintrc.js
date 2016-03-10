c9f9c8472eb7fd255f8e33d42e716e53d0f2035e
Gerad Suyderhoud <gerad@sagansystems.com>
2016-03-09 22:10:10 -0800

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "rules": {
+        "indent": [
+            2,
+            2
+        ],
+        "quotes": [
+            2,
+            "single"
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "no-console": 0,
+        "no-unused-vars": [
+          2,
+          {
+            "vars": "all",
+            "args": "none"
+          }
+        ],
+        "semi": [
+            2,
+            "always"
+        ]
+    },
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended"
+};
